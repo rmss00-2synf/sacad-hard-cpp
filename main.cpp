c2aa6a47ce1a1ab15c32a981a507e909506bd074
SYLLA <synf30@gmail.com>
2023-10-23 20:56:02 +0100

    Final version for today

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@ public:
             for(auto j = 0; j<9; j++){
                 char cell = board[i][j];
                 if(cell != '.'){
+		//We create verify and increment together
                     if(board_map[i][cell]++ > 0 || board_map[j+9][cell]++ > 0 || board_map[27 + (i/3)*3 +j/3][cell]++ > 0) return false;
-                    }
+                    		}
             }
         }
         return true;
