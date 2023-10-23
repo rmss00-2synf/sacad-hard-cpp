29319dc7519c4695aa461b8d044b6f9f66d8506b
SYLLA <synf30@gmail.com>
2023-10-23 20:41:56 +0100

    An best version

--- main.cpp
+++ main.cpp
@@ -2,28 +2,17 @@ class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
         unordered_map<int,unordered_map<char,int>> board_map;
-        int i = 0, posi, id = -1 ;
-        // Columns and rows verification
-        for(auto row : board){
-            posi = 1;
-            if(!i%3) id++;
-            for(auto cell : row){
+        for(auto i = 0; i<9; i++){
+            for(auto j = 0; j<9; j++){
+                char cell = board[i][j];
                 if(cell != '.'){
-                    board_map[i][cell]++;
-                    if(posi<=3)
-                        board_map[id+18][cell]++;
-                    else if(3<posi && posi <=6)
-                        board_map[id+19][cell]++;
-                    else if(posi>6)
-                        board_map[id+20][cell]++;
+                    if(board_map[i][cell]++ > 0) return false;
+                    if(board_map[j+9][cell]++ > 0) return false;
+                    if(board_map[27 + (i/3)*3 +j/3][cell]++ > 0) return false;
                     }
-                    posi++;
             }
-             i++;
-            for(auto map : board_map)
-                if(map > 1) return false;
-            board_map.clear();
         }
+        return true;
         
     }
 };
